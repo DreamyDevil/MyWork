b1bafd20ad024fa1bccbd76ac272257ba63dc9f0
Torgan <torganbg@gmail.com>
2021-03-05 09:31:26 +0200

    third lab

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Home from './components/pages/Home'
+import Songs from './components/pages/Songs'
+import Playlists from './components/pages/Playlists'
+import Radio from './components/pages/Radio'
+import Support from './components/pages/Support'
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vova Torgan
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+       <Router>
+         <Navbar />
+           <Switch>
+             <Route path='/' exact component={Home} />
+             <Route path='/songs'  component={Songs} />
+             <Route path='/playlists' component={Playlists} />
+             <Route path='/radio'  component={Radio} />
+             <Route path='/support' component={Support} />
+           </Switch>
+       </Router>
+     </>
   );
 }
 
---
+++ src/components/SidebarData.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as Io5Icons from "react-icons/io5";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path:'/',
+        icon: <AiIcons.AiFillHome />,
+        cName:'nav-text'
+    },
+    {
+        title: 'Songs',
+        path:'/songs',
+        icon: <FaIcons.FaMusic />,
+        cName:'nav-text'
+    },
+    {
+        title: 'Playlists',
+        path:'/playlists',
+        icon: <AiIcons.AiFillPlayCircle/>,
+        cName:'nav-text'
+    },
+    {
+        title: 'Radio',
+        path:'/radio',
+        icon: <Io5Icons.IoRadio />,
+        cName:'nav-text'
+    },
+    {
+        title: 'Support',
+        path:'/support',
+        icon: <IoIcons.IoMdHelpCircle />,
+        cName:'nav-text'
+    },
+]
